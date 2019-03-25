122271c6f9df8d8e0d0932c8628b8f3f8662f925
ByeBye-Sama <joaquin.byebye@gmail.com>
2019-03-24 20:13:22 -0500

    Finish hamburger menu with all interactions

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -16,7 +16,25 @@ $(document).ready(function () {
     e.preventDefault();
     $('.search').hide();
     $('.logo, .headernav, .miscnav').slideDown("slow");
-    
+  });
+
+  $('.js_btnMenu').on('click', function (e) {
+    e.preventDefault();
+    $('.hamburger-menu').hide();
+    $('.rsp-headernav, .share-panel, .rsp-miscnav').slideDown("slow");
+  });
+
+  $('.js_rspSearch').on('click', function (e) {
+    e.preventDefault();
+    $('.search').slideDown("slow");
+    $('.logo, .js_btnCancel').hide();
+  });
+
+  $('.js_rspCancel').on('click', function (e) {
+    e.preventDefault();
+    $('.search, .rsp-miscnav').hide();
+    $('.logo, .hamburger-menu').slideDown("slow");
+    $('.rsp-headernav, .share-panel').slideUp();
   });
 
 });
